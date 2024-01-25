8385533afa3559eb9ba1e0ca9677cb567ce610f1
santiago <saantiagosg@gmail.com>
2024-01-25 20:29:02 -0300

    primeng added

--- santiago-gauna/src/app/components/home/home.component.ts
+++ santiago-gauna/src/app/components/home/home.component.ts
@@ -1,15 +1,41 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LandingComponent } from '../landing/landing.component';
-
+import { worksData } from '../../services/api.service';
+import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
+import { TagModule } from 'primeng/tag';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.less',
-  imports: [CommonModule, RouterModule, LandingComponent],
+  imports: [CommonModule, RouterModule, LandingComponent, CarouselModule, ButtonModule, TagModule],
   standalone: true
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  data = worksData;
+  responsiveOptions: any[] | undefined;
+
+  ngOnInit(): void {
+    this.responsiveOptions = [
+      {
+        breakpoint: '1199px',
+        numVisible: 1,
+        numScroll: 1
+      },
+      {
+        breakpoint: '991px',
+        numVisible: 2,
+        numScroll: 1
+      },
+      {
+        breakpoint: '767px',
+        numVisible: 1,
+        numScroll: 1
+      }
+    ];
+  }
 
 }
